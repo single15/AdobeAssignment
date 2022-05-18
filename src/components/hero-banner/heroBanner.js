d6a3118e971af2d28f35873c79f604b53d6e00d5
single15 <s.ingle@zensar.com>
2022-05-18 13:08:20 +0530

    updated with section tag

--- src/components/hero-banner/heroBanner.js
+++ src/components/hero-banner/heroBanner.js
@@ -6,7 +6,7 @@ heroBannerTemplate.innerHTML = `
     <link rel="stylesheet" href="./components/hero-banner/heroBanner.css">
     <article class="hero-banner-container">
         <div class="aem-Grid aem-Grid--12">
-            <div class="aem-GridColumn aem-GridColumn--default--4 aem-GridColumn--tablet--6 hero-banner-heading visible-lg">
+            <section class="aem-GridColumn aem-GridColumn--default--4 aem-GridColumn--tablet--6 hero-banner-heading visible-lg">
                 <div class="new-item-tag body-3-12pt">New!</div>
                 <div class="headline-—-48pt">
                     All Natural Skin Remedies.
@@ -15,13 +15,13 @@ heroBannerTemplate.innerHTML = `
                     A healthier you from the inside out. We’ve sourced the cleanest ingredients to create a line of clean skin care treatments that leave you feeling your best
                 </div>
                 <button aria-label="Shop Product" class="btn btn-default">Shop products</button>
-            </div>
+            </section>
             
-            <div class="aem-GridColumn aem-GridColumn--phone--12 visible-xs">
+            <section class="aem-GridColumn aem-GridColumn--phone--12 visible-xs">
                 <div class="hero-banner-product-image-xs"></div>
-            </div>
+            </section>
 
-            <div class="aem-GridColumn aem-GridColumn--phone--12 hero-banner-heading visible-xs">
+            <section class="aem-GridColumn aem-GridColumn--phone--12 hero-banner-heading visible-xs">
                 <div class="headline-—-48pt">
                     All Natural Skin Remedies.
                 </div>
@@ -31,7 +31,7 @@ heroBannerTemplate.innerHTML = `
                 <div class="text-align-center">
                     <button aria-label="Shop Product" class="btn btn-default">Shop products</button>
                 </div>                
-            </div>
+            </section>
         </div>
     </article>
 `;
--- src/components/teaser/cleanProductTeaser.js
+++ src/components/teaser/cleanProductTeaser.js
@@ -6,16 +6,16 @@ teaserTemplate.innerHTML = `
     <link rel="stylesheet" href="./components/teaser/teaser.css">
     <article class="teaser-container">
         <div class="aem-Grid aem-Grid--12">
-            <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12 details-section">
+            <section class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12 details-section">
                 <div class="text-container">
                     <div class="headline-4-28pt">Clean products that deliver better solutions</div>
                     <div class="body-3-12pt">A healthier you from the inside out. We’ve sourced the cleanest ingredients to create a line of clean skin care treatments that leave you feeling your best</div>
                     <button aria-label="Add to Cart" class="btn btn-default">Shop More</button>
                 </div>
-            </div>
-            <div class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12 image-section">
+            </section>
+            <section class="aem-GridColumn aem-GridColumn--default--6 aem-GridColumn--phone--12 image-section">
                 <img aria-hidden="true"  src="../src/assets/img/man-in-blue-top-2830332@3x.png" alt="Man in blue top" width="100%" height="70%" />
-            </div>
+            </section>
         </div>
     </article>
 `;
